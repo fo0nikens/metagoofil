d7eab3b85fb4eb8b11ddf85e2af59cfe9919d4ee
maldevel <maldevel@mail.com>
2016-02-01 21:21:12 +0200

    store links into results/domain folder
    store links into results/domain folder

--- core/harvester.py
+++ core/harvester.py
@@ -120,7 +120,7 @@ class Metagoofil:
             
             # Save links to output to file
             if self.saveLinks:
-                self.f = open(os.path.join(os.getcwd(), 'results', 'html_links_' + self.get_timestamp() + '_' + self.domain + '.txt'), 'a')
+                self.f = open(os.path.join(os.getcwd(), 'results', self.domain, 'html_links_' + self.get_timestamp() + '_' + self.domain + '.txt'), 'a')
                 for f in self.files:
                     self.f.write(f + "\n")
                 self.f.close()
